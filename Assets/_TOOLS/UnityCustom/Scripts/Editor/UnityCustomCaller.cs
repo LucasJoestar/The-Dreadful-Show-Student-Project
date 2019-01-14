15a4d23e1374010163ca0755bdc276d5f4740506
Will <william.comminges@gmail.com>
2019-01-14 11:53:22 +0100

    Add tool perso

---
+++ Assets/_TOOLS/UnityCustom/Scripts/Editor/UnityCustomCaller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class UnityCustomCaller : MonoBehaviour
+{
+    [MenuItem("Tools/CustomUnity/Change camera")]
+    public static void CallManager()
+    {
+        if (Camera.main)
+        {
+            DestroyImmediate(Camera.main.gameObject);
+        }
+        Instantiate(Resources.Load("CustomMainCamera"));
+        Debug.Log("Camera custom is now on the scene.");
+    }
+}
