c5f8e6504684eb291cf273221e1b61bdae95099e
Lucas <lucasjoestar@outlook.fr>
2019-01-14 17:26:39 +0100

    Création de classes
    Création des classes Damageable, Character, ainsi que du script contenant tous les enums, EnumsHolder

---
+++ Assets/Scripts/_TEAM/TDS_EnumsHolder.cs
@@ -0,0 +1,21 @@
+﻿/* TDS_EnumsHolder :
+ *
+ *	#####################
+ *	###### PURPOSE ######
+ *	#####################
+ *
+ *	References all enums used for the project "The Dreadful Show"
+ *
+ *	#####################
+ *	### MODIFICATIONS ###
+ *	#####################
+ *
+ *	Date :			[DATE]
+ *	Author :		[NAME]
+ *
+ *	Changes :
+ *
+ *	[CHANGES]
+ *
+ *	-----------------------------------
+*/
