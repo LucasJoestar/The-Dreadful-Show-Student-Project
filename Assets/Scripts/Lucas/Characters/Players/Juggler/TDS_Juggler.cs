833085298fe540295c5f15c92f4061438f6f5d44
Lucas <lucasjoestar@outlook.fr>
2019-02-07 12:04:57 +0100

    Test d'éditeur en Dark Skin

--- Assets/Scripts/Lucas/Characters/Players/Juggler/TDS_Juggler.cs
+++ Assets/Scripts/Lucas/Characters/Players/Juggler/TDS_Juggler.cs
@@ -81,19 +81,19 @@ public class TDS_Juggler : TDS_Player
     // Awake is called when the script instance is being loaded
     protected override void Awake()
     {
-
+        base.Awake();
     }
 
     // Use this for initialization
     protected override void Start()
     {
-
+        base.Start();
     }
 
     // Update is called once per frame
     protected override void Update()
     {
-        
+        base.Update();
 	}
 	#endregion
 
