89b376da80718a5eaaf684d64e6b3c87b230426f
Will <william.comminges@gmail.com>
2019-02-19 09:27:35 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Will/NetWork/TDS_RPCManager.cs
+++ Assets/Scripts/Will/NetWork/TDS_RPCManager.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TDS_RPCManager : MonoBehaviour 
+public class TDS_RPCManager : MonoBehaviour
 {
     /* TDS_RPCManager :
 	 *
