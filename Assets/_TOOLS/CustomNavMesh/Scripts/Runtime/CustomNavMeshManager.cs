9a64c051c43344317c241ce81af43cdb5ba04b2b
Alexis <thiebautalexis01@gmail.com>
2019-03-18 14:24:57 +0100

    Modif scenes

--- Assets/_TOOLS/CustomNavMesh/Scripts/Runtime/CustomNavMeshManager.cs
+++ Assets/_TOOLS/CustomNavMesh/Scripts/Runtime/CustomNavMeshManager.cs
@@ -50,7 +50,7 @@ public class CustomNavMeshManager
         TextAsset _textDatas = Resources.Load(Path.Combine(ResourcesPath, _fileName), typeof(TextAsset)) as TextAsset;
         if(_textDatas == null)
         {
-            Debug.LogError($"{_fileName} not found.");
+            Debug.LogWarning($"{_fileName} not found.");
             return; 
         }
         CustomNavDataSaver<CustomNavData> _loader = new CustomNavDataSaver<CustomNavData>();
