66cb8fe00c987fd49ad4f801d6acc4c20e25ce5b
Will <william.comminges@gmail.com>
2019-03-21 11:33:23 +0100

    GoTo LW Pipeline

---
+++ Assets/GlowTool/Scripts/Core/GlowManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowManager : MonoBehaviour
+{
+
+    #region F/P
+    public List<GameObject> AllObjectsToMakeGlowy = new List<GameObject>();
+    #endregion
+
+    #region Methods
+    public void AddObject(GameObject _go)
+    {
+        if (!_go) return;
+        AllObjectsToMakeGlowy.Add(_go);
+    }
+
+    public void RemoveAllObjects() => AllObjectsToMakeGlowy.Clear();
+
+    public void RemoveObjectAt(int _o) => AllObjectsToMakeGlowy.RemoveAt(_o);
+    #endregion
+}
