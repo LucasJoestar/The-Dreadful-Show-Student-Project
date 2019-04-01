132843833ffe2e5368d85f5e04873aeb6622706b
Lucas Guibert <lucasjoestar@outlook.fr>
2019-04-01 17:47:04 +0200

    Préparation de la création du niveau principal
    Mise à jour de divers assets, création des LevelManager & LevelBounds, ajustement de la roulade... Pleins de petites choses

---
+++ Assets/Scripts/Lucas/TDS_LevelBounds.cs
@@ -0,0 +1,119 @@
+﻿using UnityEngine;
+
+public class TDS_LevelBounds : MonoBehaviour 
+{
+    /* TDS_LevelBounds :
+	 *
+	 *	#####################
+	 *	###### PURPOSE ######
+	 *	#####################
+	 *
+	 *	Activate bounds for the player & the camera when entering trigger,
+     *	and set this as current bounds in the Level Manager ;
+     *	desactivate them with a public method.
+	 *
+	 *	#####################
+	 *	####### TO DO #######
+	 *	#####################
+	 *
+	 *	Hum...
+	 *
+	 *	#####################
+	 *	### MODIFICATIONS ###
+	 *	#####################
+	 *
+	 *	Date :			[01 / 04 / 2019]
+	 *	Author :		[Guibert Lucas]
+	 *
+	 *	Changes :
+	 *
+	 *	Creation of the TDS_LevelBounds class.
+	 *
+	 *	-----------------------------------
+	*/
+
+    #region Fields / Properties
+    /// <summary>
+    /// Bounds of the camera to set as new when get activated.
+    /// </summary>
+    [SerializeField] private TDS_Bounds cameraBounds = null;
+
+    /// <summary>Public accessor of <see cref="cameraBounds"/>.</summary>
+    public TDS_Bounds CameraBounds { get { return cameraBounds; } }
+
+    /// <summary>
+    /// Collider trigger to enable these bounds.
+    /// </summary>
+    [SerializeField] private new BoxCollider collider = null;
+
+    /// <summary>
+    /// Child parent of all colliders to activate with the bounds.
+    /// </summary>
+    [SerializeField] private GameObject colliderChild = null;
+
+    /// <summary>Public accessor of <see cref="colliderChild"/>.</summary>
+    public GameObject ColliderChild { get { return colliderChild; } }
+    #endregion
+
+    #region Methods
+
+    #region Original Methods
+    /// <summary>
+    /// Activate these bounds.
+    /// </summary>
+    public void Activate()
+    {
+        TDS_Camera.Instance.SetBounds(cameraBounds);
+        colliderChild.SetActive(true);
+        collider.enabled = false;
+    }
+
+    /// <summary>
+    /// Desactivate these bounds.
+    /// </summary>
+    public void Desactivate()
+    {
+        TDS_Camera.Instance.SetBounds(null);
+        gameObject.SetActive(false);
+    }
+	#endregion
+
+	#region Unity Methods
+	// Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+
+    }
+
+    // OnTriggerEnter is called when the GameObject collides with another GameObject
+    private void OnTriggerEnter(Collider other)
+    {
+        Activate();
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        // Try to get missing references
+		if (!colliderChild)
+        {
+            colliderChild = transform.GetChild(0).gameObject;
+            if (!colliderChild) Debug.LogWarning($"The Bounds \"{name}\" doesn't have any child colliders attached !");
+        }
+        if (!collider)
+        {
+            collider = transform.GetComponent<BoxCollider>();
+            if (!collider) Debug.LogWarning($"The Bounds \"{name}\" collider is missing !");
+        }
+        else if (!collider.isTrigger) collider.isTrigger = true;
+    }
+	
+	// Update is called once per frame
+	private void Update()
+    {
+        
+	}
+	#endregion
+
+	#endregion
+}
---
+++ Assets/Scripts/Lucas/TDS_LevelManager.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class TDS_LevelManager : MonoBehaviour 
+{
+    /* TDS_LevelManager :
+	 *
+	 *	#####################
+	 *	###### PURPOSE ######
+	 *	#####################
+	 *
+	 *	Manages the behaviour of a level.
+	 *
+	 *	#####################
+	 *	####### TO DO #######
+	 *	#####################
+	 *
+	 *	... Mhmm...
+	 *
+	 *	#####################
+	 *	### MODIFICATIONS ###
+	 *	#####################
+	 *
+	 *	Date :			[01 / 04 / 2019]
+	 *	Author :		[Guibert Lucas]
+	 *
+	 *	Changes :
+	 *
+	 *	Creation of the TDS_LevelManager class.
+	 *
+	 *	-----------------------------------
+	*/
+
+    #region Events
+    /// <summary>
+    /// Event called when a new checkpoint is set.
+    /// </summary>
+    public Action<TDS_Checkpoint> OnCheckpointActivated = null;
+    #endregion
+
+    #region Fields / Properties
+    /// <summary>
+    /// Last activated checkpoint.
+    /// </summary>
+    [SerializeField] private TDS_Checkpoint checkpoint = null;
+
+    /// <summary>Public accessor for <see cref="checkpoint"/>.</summary>
+    public TDS_Checkpoint Checkpoint { get { return checkpoint; } }
+
+    /// <summary>
+    /// Local player, the one who play on this machine.
+    /// </summary>
+    [SerializeField] private TDS_Player localPlayer = null;
+
+    /// <summary>
+    /// Online players, the ones that play with the one playing on this machine.
+    /// </summary>
+    [SerializeField] private List<TDS_Player> onlinePlayers = new List<TDS_Player>();
+
+    /// <summary>
+    /// Get all players of the game, local and online ones.
+    /// </summary>
+    public TDS_Player[] AllPlayers
+    {
+        get { return onlinePlayers.Append(localPlayer).ToArray(); }
+    }
+
+    /// <summary>
+    /// Points where to spawn at game start.
+    /// </summary>
+    public Vector3[] StartSpawnPoints = new Vector3[] { };
+    #endregion
+
+    #region Proto
+    [SerializeField] private GameObject player = null;
+    #endregion
+
+    #region Singleton
+    /// <summary>
+    /// Singleton instance of this class.
+    /// </summary>
+    public static TDS_LevelManager Instance = null;
+    #endregion
+
+    #region Methods
+
+    #region Original Methods
+    /// <summary>
+    /// Make local player spawn.
+    /// </summary>
+    public void Spawn()
+    {
+        // Test things
+        localPlayer = Instantiate(player, StartSpawnPoints[0], Quaternion.identity).GetComponentInChildren<TDS_Player>();
+        TDS_Camera.Instance.Target = localPlayer.transform;
+
+        localPlayer.OnDie += Respawn;
+
+        // Instantiate the player
+        PlayerType _type = player.GetComponentInChildren<TDS_Player>().PlayerType;
+        //TDS_NetworkManager.Instance.InstantiatePlayer(_type);
+    }
+
+    /// <summary>
+    /// Make all dead players respawn.
+    /// </summary>
+    private void Respawn()
+    {
+        if (checkpoint) checkpoint.Respawn();
+        else
+        {
+            // Test use
+            TDS_Player[] _deadPlayers = AllPlayers.Where(p => p.IsDead).ToArray();
+
+            if (_deadPlayers.Length == 0) return;
+
+            TDS_Player _player = null;
+
+            for (int _i = 0; _i < _deadPlayers.Length; _i++)
+            {
+                _player = _deadPlayers[_i];
+
+                _player.transform.position = StartSpawnPoints[_i];
+                _player.HealthCurrent = _player.HealthMax;
+                _player.gameObject.SetActive(true);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Set new checkpoint where to respawn.
+    /// </summary>
+    /// <param name="_checkpoint">Checkpoint to set as new one.</param>
+    public void SetCheckpoint(TDS_Checkpoint _checkpoint)
+    {
+        if (_checkpoint == null) return;
+
+        checkpoint = _checkpoint;
+        OnCheckpointActivated?.Invoke(_checkpoint);
+
+        // Make dead players respawn
+        Respawn();
+    }
+    #endregion
+
+    #region Unity Methods
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+        // Set the singleton instance if null
+        if (!Instance) Instance = this;
+        else Destroy(this);
+    }
+
+    // Destroying the attached Behaviour will result in the game or Scene receiving OnDestroy
+    private void OnDestroy()
+    {
+        // Nullify the singleton instance if needed
+        if (Instance == this) Instance = null;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        TDS_NetworkManager.Instance.InitConnection();
+
+        // Spawn local player.
+        //Spawn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #endregion
+}
