19dc3e93b227adc66b84a95651b40a6ce4bf89d8
Azy <william.comminges@gmail.com>
2019-05-19 23:06:17 +0200

    blabla

--- Assets/Scripts/Will/NetWork/TDS_LobbyManager.cs
+++ Assets/Scripts/Will/NetWork/TDS_LobbyManager.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon;
 
-public class TDS_LobbyManager : MonoBehaviour
+public class TDS_LobbyManager : PunBehaviour
 {
     /* TDS_RPCManager :
 	 *
@@ -29,16 +30,42 @@ public class TDS_LobbyManager : MonoBehaviour
     #endregion
 
     #region Fields / Properties
-
+    [SerializeField]
+    string connectionVersion = "1.1";
     #endregion
 
     #region Methods
     #region Original Methods   
-    
+    private void Init()
+    {
+       if(!PhotonNetwork.connected)
+        {
+            PhotonNetwork.autoJoinLobby = false;
+            PhotonNetwork.automaticallySyncScene = true;
+            PhotonNetwork.ConnectUsingSettings(connectionVersion);
+        }
+    }
+    #endregion
+
+    #region Photon Methods
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("connect to master");
+        //PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("connect to Lobby");
+    }
+
     #endregion
 
     #region Unity Methods
-   
+    private void Start()
+    {
+        Init();
+    }
     #endregion
     #endregion
 }
