ee8125a284e4ddb09bc47c257cd5dc0464b94505
Azy <william.comminges@gmail.com>
2019-05-20 02:01:20 +0200

    up multi

--- Assets/Scripts/Will/NetWork/TDS_LobbyManager.cs
+++ Assets/Scripts/Will/NetWork/TDS_LobbyManager.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using Photon;
+using UnityEngine;
+using UnityEngine.UI;
+
 
 public class TDS_LobbyManager : PunBehaviour
 {
@@ -31,11 +33,34 @@ public class TDS_LobbyManager : PunBehaviour
 
     #region Fields / Properties
     [SerializeField]
-    string connectionVersion = "1.1";
+    string connectionVersion = "2.1";
+    [SerializeField]
+    GameObject[] entryButtons;
     #endregion
 
     #region Methods
     #region Original Methods   
+    public void CreateRoom()
+    {
+        PhotonNetwork.CreateRoom("Room Name", new RoomOptions() { MaxPlayers = 4 }, null);
+        Debug.Log("Creating N Joining room !!");
+    }
+
+    public void GetRoomList()
+    {
+        RoomInfo[] _allRoom = PhotonNetwork.GetRoomList();
+        if(_allRoom.Length > 0)
+        {
+            for (int i = 0; i < _allRoom.Length; i++)
+            {
+                RoomInfo _room = _allRoom[i];
+                GameObject _entry = entryButtons[i];
+                Text _entryText = _entry.GetComponentInChildren<Text>();
+                _entryText.text = _room.Name;
+            }
+        }
+    }
+
     private void Init()
     {
        if(!PhotonNetwork.connected)
@@ -45,13 +70,18 @@ public class TDS_LobbyManager : PunBehaviour
             PhotonNetwork.ConnectUsingSettings(connectionVersion);
         }
     }
+
+    public void JoinRoom(Text _entryText)
+    {
+        PhotonNetwork.JoinRoom(_entryText.text);
+    }
     #endregion
 
     #region Photon Methods
     public override void OnConnectedToMaster()
     {
         Debug.Log("connect to master");
-        //PhotonNetwork.JoinLobby();
+        PhotonNetwork.JoinLobby();
     }
 
     public override void OnJoinedLobby()
@@ -59,6 +89,16 @@ public class TDS_LobbyManager : PunBehaviour
         Debug.Log("connect to Lobby");
     }
 
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("Joined room");
+    }
+
+    public override void OnPhotonPlayerConnected(PhotonPlayer _newPlayer)
+    {
+        Debug.Log(_newPlayer.NickName + "joined the room");
+        if (PhotonNetwork.isMasterClient && PhotonNetwork.room.PlayerCount == 4) PhotonNetwork.LoadLevel(1);
+    }
     #endregion
 
     #region Unity Methods
