fa5c1760262c3d8ff5677e30805f43123cf4d41b
Alexis <thiebautalexis01@gmail.com>
2019-05-22 14:07:55 +0200

    Push Pre AttackBehaviour

---
+++ Assets/Scripts/Alexis/Enemy/Boss/TDS_BrutalSiamese.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TDS_BrutalSiamese : TDS_Enemy 
+{
+    /* TDS_BrutalSiamese :
+ *
+ *	#####################
+ *	###### PURPOSE ######
+ *	#####################
+ *
+ *	[PURPOSE]
+ *
+ *	#####################
+ *	####### TO DO #######
+ *	#####################
+ *
+ *	[TO DO]
+ *
+ *	#####################
+ *	### MODIFICATIONS ###
+ *	#####################
+ *
+ *	Date :			[DATE]
+ *	Author :		[NAME]
+ *
+ *	Changes :
+ *
+ *	[CHANGES]
+ *
+ *	-----------------------------------
+*/
+
+    #region Events
+
+    #endregion
+
+    #region Fields / Properties
+
+    #endregion
+
+    #region Methods
+
+    #region Original Methods
+
+    #endregion
+
+    #region Overriden Methods
+    public override void ActivateEnemy()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override void ActivateAttack(int _animationID)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override bool AttackCanBeCasted()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override float GetMaxRange()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override float GetMinRange()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override float StartAttack(float _distance)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+    #region Unity Methods
+    // Awake is called when the script instance is being loaded
+    private void Awake()
+    {
+
+    }
+
+	// Use this for initialization
+    private void Start()
+    {
+		
+    }
+	
+	// Update is called once per frame
+	private void Update()
+    {
+        
+	}
+	#endregion
+
+	#endregion
+}
