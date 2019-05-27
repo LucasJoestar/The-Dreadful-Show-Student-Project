6e185b34c660ed13393e8fb3e41d7482737117bb
Will <william.comminges@gmail.com>
2019-05-27 17:06:04 +0200

    up enum holder + room selection

--- Assets/Scripts/Will/NetWork/TDS_LobbyManager.cs
+++ Assets/Scripts/Will/NetWork/TDS_LobbyManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using Photon;
+using System;
 
 public class TDS_LobbyManager : PunBehaviour
 {
@@ -32,11 +33,46 @@ public class TDS_LobbyManager : PunBehaviour
 
     #region Methods
     #region Original Methods   
+    public void CreateRoom(Button _btn)
+    {
+        RoomId _roomId;
+
+        _roomId = _btn.name == "FirstRoom" ? RoomId.FirstRoom :
+                  _btn.name == "SecondRoom" ? RoomId.SecondRoom :
+                  _btn.name == "ThirdRoom" ? RoomId.ThirdRoom :
+                  _btn.name == "FourthRoom" ? RoomId.FourthRoom :
+                  _btn.name == "FifthRoom" ? RoomId.FifthRoom : 
+                  RoomId.WaitForIt;
+
+        if (_roomId == RoomId.WaitForIt)
+        {
+            Debug.LogError("Can't connect to the room");
+            return;
+        }
+
+        InitLobbyConnection(_roomId);
+    }
+
+    void InitLobbyConnection(RoomId _roomId)
+    {
+        int _getIndex = (int)_roomId;
+        string _stringID = _getIndex.ToString();
+
+        if (!PhotonNetwork.connected)
+        {
+            PhotonNetwork.autoJoinLobby = false;
+            PhotonNetwork.automaticallySyncScene = true;
+            PhotonNetwork.ConnectUsingSettings(_stringID);
+        }
+    }
+    #endregion
+
+    #region Photon Methods   
 
     #endregion
 
     #region Unity Methods
-
+    
     #endregion
     #endregion
 }
