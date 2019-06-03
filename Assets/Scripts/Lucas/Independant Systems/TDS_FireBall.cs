eaa0e105e91f102b3dac8135cfc259c5d2042176
Lucas Guibert <lucasjoestar@outlook.fr>
2019-06-03 17:33:24 +0200

    FIIIIIx

--- Assets/Scripts/Lucas/Independant Systems/TDS_FireBall.cs
+++ Assets/Scripts/Lucas/Independant Systems/TDS_FireBall.cs
@@ -148,8 +148,6 @@ public class TDS_FireBall : MonoBehaviour
     // Use this for initialization
     private void Start()
     {
-        if (!photonView.isMine) return;
-
         StartCoroutine(MakeTrajectory());
     }
 	#endregion
