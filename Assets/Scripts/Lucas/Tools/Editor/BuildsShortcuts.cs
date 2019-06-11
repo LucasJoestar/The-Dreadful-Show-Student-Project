2f7fd167099f72cd3118ced6c836c3034871af83
Lucas Guibert <lucasjoestar@outlook.fr>
2019-06-11 17:55:24 +0200

    La ville

---
+++ Assets/Scripts/Lucas/Tools/Editor/BuildsShortcuts.cs
@@ -0,0 +1,110 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using Debug = UnityEngine.Debug;
+
+public class BuildsShortcuts : MonoBehaviour
+{
+    /* OpenLastBuildFolder :
+	*
+	* A series of shortcuts integrated in the Unity's top menu bar
+    * that have for purpose to facilitate the access to builds
+    * (Folders & .exe)
+    * 
+    * Contains 3 shortucts :
+    *       - Launch the last build
+    *       - Open the builds folder
+    *       - Open the last build directory
+	*/
+
+    #region Fields
+    // The default builds folder
+    public static string buildsFolder = (Application.dataPath.Replace("/Assets", string.Empty) + "/Build");
+    #endregion
+
+    #region Methods
+
+    #region Menu Shortcuts
+    // Launches the last build made (.exe file)
+    [MenuItem("Tools/Builds Shortcuts/Launch Last Build")]
+    public static void LaunchLastBuild()
+    {
+        // Creates the builds folder if it does not already exists
+        CreateDirectory(buildsFolder);
+
+        // Get all builds folder
+        string[] _buildFolders = Directory.GetDirectories(buildsFolder);
+
+        // If at least one folder is found, try to find the .exe file in the last edited one
+        if (_buildFolders != null && _buildFolders.Length > 0)
+        {
+            // Get the last edited folder
+            string _buildFolder = _buildFolders.OrderBy(f => Directory.GetLastWriteTime(f)).Last();
+
+            // Get the .exe file path
+            string _exeFilePath = _buildFolder + '/' + Application.productName + ".exe";
+
+            // If the .exe file exists, launch it
+            if (File.Exists(_exeFilePath))
+            {
+                Process.Start(_exeFilePath);
+            }
+            // If it does not exist, debug it and open the build directory
+            else
+            {
+                Debug.LogWarning("There is no .exe file in the last build folder or it has a different name !");
+
+                Process.Start(_buildFolder);
+            }
+        }
+        // If there's no folder, debug it
+        else
+        {
+            Debug.LogWarning("There is no build in the default build folder !");
+        }
+    }
+
+    // Opens the default build folder
+    [MenuItem("Tools/Builds Shortcuts/Open Builds Folder")]
+    public static void OpenBuildsFolder()
+    {
+        // Creates the builds folder if it does not already exists
+        CreateDirectory(buildsFolder);
+
+        // Open the builds folder
+        Process.Start(buildsFolder);
+    }
+
+    // Opens the last build made directory
+    [MenuItem("Tools/Builds Shortcuts/Open Last Build Directory")]
+	public static void OpenLastBuildDirectory()
+    {
+        // Creates the builds folder if it does not already exists
+        CreateDirectory(buildsFolder);
+
+        // Get all builds folder
+        string[] _buildFolders = Directory.GetDirectories(buildsFolder);
+
+        // If at least one folder is found, open the last edited one
+        if (_buildFolders != null && _buildFolders.Length > 0)
+        {
+            Process.Start(_buildFolders.OrderBy(f => Directory.GetLastWriteTime(f)).Last());
+        }
+        // If there's no folder, debug it
+        else
+        {
+            Debug.LogWarning("There is no build in the default build folder !");
+        }
+    }
+    #endregion
+
+    // Creates a directory path if it does not already exists
+    private static void CreateDirectory(string _path)
+    {
+        if (!Directory.Exists(_path)) Directory.CreateDirectory(_path);
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Lucas/Tools/Editor/TDS_SceneEditorUtility.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.IO;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public static class TDS_SceneEditorUtility
+{
+    /* TDS_SceneEditorUtility# :
+	 *
+	 *	#####################
+	 *	###### PURPOSE ######
+	 *	#####################
+	 *
+	 *	Class to centralize scene-related tools shortcuts.
+	 *
+	 *	#####################
+	 *	####### TO DO #######
+	 *	#####################
+	 *
+	 *	...
+	 *
+	 *	#####################
+	 *	### MODIFICATIONS ###
+	 *	#####################
+	 *
+	 *	Date :			[11 / 06 / 2019]
+	 *	Author :		[Guibert Lucas]
+	 *
+	 *	Changes :
+	 *
+	 *	Creation of the TDS_SceneEditorUtility class.
+	 *
+	 *	-----------------------------------
+	*/
+
+    #region Fields / Properties
+
+    #endregion
+
+    #region Methods
+    /// <summary>
+    /// Launch the whole game from the first scene in build index.
+    /// </summary>
+    /*[MenuItem("Tools/Scenes/Launch Game from Start")]
+    public static void StartGame()
+    {
+        //
+    }*/
+    #endregion
+}
+
+public class TDS_LoadSceneWindow : EditorWindow
+{
+    /* TDS_LoadSceneWindow# :
+	 *
+	 *	#####################
+	 *	###### PURPOSE ######
+	 *	#####################
+	 *
+	 *	Window allowing to load any project scene.
+	 *
+	 *	#####################
+	 *	####### TO DO #######
+	 *	#####################
+	 *
+	 *	...
+	 *
+	 *	#####################
+	 *	### MODIFICATIONS ###
+	 *	#####################
+	 *
+	 *	Date :			[11 / 06 / 2019]
+	 *	Author :		[Guibert Lucas]
+	 *
+	 *	Changes :
+	 *
+	 *	Creation of the TDS_LoadSceneWindow.
+	 *
+	 *	-----------------------------------
+	*/
+
+    #region Fields / Properties
+    /// <summary>
+    /// All project loaded scenes path.
+    /// </summary>
+    private string[] allScenesPath = new string[] { };
+    #endregion
+
+    #region Methods
+
+    #region Original Methods
+    /// <summary>
+    /// Call this window from the Unity menu toolbar.
+    /// </summary>
+    [MenuItem("Tools/Scenes/Load Scene Menu")]
+    public static void CallWindow()
+    {
+        GetWindow(typeof(TDS_LoadSceneWindow), true, "Load Scene Window").Show();
+    }
+    #endregion
+
+    #region Unity Methods
+    // This function is called when the object is loaded
+    private void OnEnable()
+    {
+        allScenesPath = Array.ConvertAll<string, string>(AssetDatabase.FindAssets("t:Scene"), AssetDatabase.GUIDToAssetPath);
+        Array.Sort(allScenesPath);
+
+        maxSize = new Vector2(275, (allScenesPath.Length * 20) + 5);
+        minSize = maxSize;
+    }
+
+    // Implement your own editor GUI here
+    private void OnGUI()
+    {
+        foreach (string _scene in allScenesPath)
+        {
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUILayout.LabelField(Path.GetFileNameWithoutExtension(_scene));
+            GUILayout.FlexibleSpace();
+            if (GUILayout.Button("Load"))
+            {
+                EditorSceneManager.OpenScene(_scene);
+                Close();
+                return;
+            }
+
+            EditorGUILayout.EndHorizontal();
+        }
+    }
+    #endregion
+
+    #endregion
+}
