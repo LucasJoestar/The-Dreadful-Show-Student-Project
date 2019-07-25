4cb9d9f4725c0c0eae64b012b5c492e9384ed50c
Alexis <thiebautalexis01@gmail.com>
2019-07-25 17:58:48 +0200

    Change Animation Methods

--- Assets/Scripts/Lucas/Attacks/TDS_AttackEffect.cs
+++ Assets/Scripts/Lucas/Attacks/TDS_AttackEffect.cs
@@ -154,7 +154,7 @@ public class TDS_AttackEffect
                 _target.BringCloser(_target.transform.position.x - _attacker.transform.position.x);
                 if(_attacker is TDS_Enemy)
                 {
-                    ((TDS_Enemy)_attacker).SetAnimationTrigger("BringTargetCloser");
+                    ((TDS_Enemy)_attacker).SetAnimationState((int)EnemyAnimationState.BringTargetCloser);
                     ((TDS_Enemy)_attacker).BringingTarget = _target; 
                     _target.OnStopBringingCloser += ((TDS_Enemy)_attacker).TargetBrought; 
                 }
