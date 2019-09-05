9e0fcc7d3621b5188ade8b12e9c6ddf17d15676f
Will <william.comminges@gmail.com>
2019-09-05 18:08:36 +0200

    Light Behavior

---
+++ Assets/Scripts/Will/Lights/TDS_LightBehavior.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections; 
+
+[ExecuteAlways]
+public class TDS_LightBehavior : MonoBehaviour
+{
+    #region Fields/Properties
+    #region Light
+    [Space]
+    Light lightCustom;
+    float lightInt;
+    [SerializeField]
+    float minInt = 3f;
+    [SerializeField]
+    float maxInt = 5f;
+
+    [SerializeField]
+    float changeLightRate = 1;
+
+    private Coroutine lightCoroutine = null; 
+    #endregion
+    #endregion
+
+    #region Methods   
+    #region Light
+    void ChangeLightIntensity()
+    {
+        if(lightCoroutine != null)
+        {
+            StopCoroutine(lightCoroutine);
+            lightCoroutine = null; 
+        }
+        lightCoroutine = StartCoroutine(UpdateLightIntensity()); 
+    }
+
+    IEnumerator UpdateLightIntensity()
+    {
+        lightInt = Random.Range(minInt, maxInt);
+        float _originalIntensity = lightCustom.intensity;
+        float _delta = 0;
+        while (_delta < changeLightRate)
+        {
+            lightCustom.intensity = Mathf.Lerp(_originalIntensity, lightInt, _delta / changeLightRate); 
+            yield return null;
+            _delta += Time.deltaTime;
+        }
+        lightCoroutine = null;
+        ChangeLightIntensity(); 
+    }
+    #endregion
+    #endregion
+
+    #region UnityMethods
+    void Start()
+    {
+        lightCustom = GetComponent<Light>();
+        ChangeLightIntensity(); 
+    }
+
+    #endregion
+}
