95eedb39c2a7b9c8596f6af91abc0ffcac0aef3a
Blast-SD <matthieurauber@gmail.com>
2020-05-20 22:17:57 +0200

    division des banques

---
+++ The Dreadful Show_wwise_project/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,118 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID ACROBAT = 248488741U;
+        static const AkUniqueID BRUTAL_SIAMESE = 2390245931U;
+        static const AkUniqueID FAKIR = 2888865648U;
+        static const AkUniqueID MIGHTYMAN = 2358710769U;
+        static const AkUniqueID MIME = 2559194501U;
+        static const AkUniqueID PLAY_AMBIANCE_PLUIE = 3995323232U;
+        static const AkUniqueID PLAY_BODYFALL = 1500695173U;
+        static const AkUniqueID PLAY_BRUTAL_HIT = 812881236U;
+        static const AkUniqueID PLAY_CIRCUS_AMB = 3809599778U;
+        static const AkUniqueID PLAY_DODGE = 3338421947U;
+        static const AkUniqueID PLAY_DRINK = 4042538376U;
+        static const AkUniqueID PLAY_DRUNK = 4045789892U;
+        static const AkUniqueID PLAY_EAT = 2489068396U;
+        static const AkUniqueID PLAY_FIREBALL_HIT = 3177064491U;
+        static const AkUniqueID PLAY_FIREBALL_SPIT = 2915545918U;
+        static const AkUniqueID PLAY_FIREBREATH = 694398836U;
+        static const AkUniqueID PLAY_FOOT_BEARLADY = 99002451U;
+        static const AkUniqueID PLAY_FOOT_DAME = 3677433246U;
+        static const AkUniqueID PLAY_FOOT_FIREEATER = 1740506432U;
+        static const AkUniqueID PLAY_FOOT_JUGLER = 114210310U;
+        static const AkUniqueID PLAY_HUD_BIGCONFIRM = 944420934U;
+        static const AkUniqueID PLAY_HUD_CONFIRM = 2196985468U;
+        static const AkUniqueID PLAY_HUD_OVER = 138578522U;
+        static const AkUniqueID PLAY_HUD_READY = 1110856793U;
+        static const AkUniqueID PLAY_JUGGLE = 212900658U;
+        static const AkUniqueID PLAY_LAND_JUMP = 169396492U;
+        static const AkUniqueID PLAY_LARGE_BLOW = 1082762910U;
+        static const AkUniqueID PLAY_LOCK = 4064908091U;
+        static const AkUniqueID PLAY_LOOP_FORET = 3908812351U;
+        static const AkUniqueID PLAY_PARRY = 4115969904U;
+        static const AkUniqueID PLAY_SLIDE = 3715669161U;
+        static const AkUniqueID PLAY_SMALL_BLOW = 134550002U;
+        static const AkUniqueID PLAY_SPIT = 301125896U;
+        static const AkUniqueID PLAY_SPLAT = 708322162U;
+        static const AkUniqueID PLAY_START_JUMP = 406111645U;
+        static const AkUniqueID PLAY_TORCH_OFF = 3595911238U;
+        static const AkUniqueID PLAY_TORCH_ON = 505828200U;
+        static const AkUniqueID PLAY_TORNADO = 369801413U;
+        static const AkUniqueID PLAY_VOMIT = 1339992111U;
+        static const AkUniqueID PUNK = 1942555113U;
+        static const AkUniqueID SIAMESE = 804534660U;
+        static const AkUniqueID STOP_AMBIANCE_PLUIE = 2819974902U;
+        static const AkUniqueID STOP_DRUNK = 411907754U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace ENNEMIS_ATTACK_SWITCH
+        {
+            static const AkUniqueID GROUP = 2066487578U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID ATTAQUE_1 = 4145357458U;
+                static const AkUniqueID ATTAQUE_2 = 4145357457U;
+                static const AkUniqueID ATTAQUE_3 = 4145357456U;
+                static const AkUniqueID ATTAQUE_4 = 4145357463U;
+                static const AkUniqueID ATTAQUE_5 = 4145357462U;
+                static const AkUniqueID ATTAQUE_6 = 4145357461U;
+            } // namespace SWITCH
+        } // namespace ENNEMIS_ATTACK_SWITCH
+
+        namespace FOOTSTEPS_SWITCH
+        {
+            static const AkUniqueID GROUP = 3140091853U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID CONCRETE = 841620460U;
+                static const AkUniqueID DIRT = 2195636714U;
+                static const AkUniqueID WOOD = 2058049674U;
+            } // namespace SWITCH
+        } // namespace FOOTSTEPS_SWITCH
+
+    } // namespace SWITCHES
+
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID ENNEMIES_ATTACK = 3540893800U;
+        static const AkUniqueID FOOT_SURFACE = 2961358289U;
+    } // namespace GAME_PARAMETERS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID AMBIANCES = 1404066300U;
+        static const AkUniqueID LA_SEULE_ET_UNIQUE_BANQUE_DE_SOON = 3151042580U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
